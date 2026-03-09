21fbe0316416d4118eb1782e3c4550e3b5146b42
mozo-web <monica25mozo@gmail.com>
2026-03-09 08:38:57 -0600

    Update server.js

--- server.js
+++ server.js
@@ -4,8 +4,10 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hola desde Render 🚀");
+  res.send("Hola desde Render ");
 });
+
+app.use(express.static("public"));
 
 app.listen(PORT, () => {
   console.log("Servidor corriendo en puerto " + PORT);
